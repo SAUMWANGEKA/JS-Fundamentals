f2d812ea21fb807ed62ace906ac2040bbd999331
SAUMWANGEKA <146136230+SAUMWANGEKA@users.noreply.github.com>
2025-07-06 22:29:04 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,11 @@
 const arg = parseInt(process.argv[2]);
 
-if (isNaN(arg)) {
+if (isNaN(arg) || arg < 0) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < arg; i++) {
+  let i = 0;
+  while (i < arg) {
     console.log('C is fun');
+    i++;
   }
 }
