98b8b62400cc8835ad7f6ff36a74f09b396f250a
SAUMWANGEKA <146136230+SAUMWANGEKA@users.noreply.github.com>
2025-07-06 22:35:02 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -7,24 +7,3 @@ if (isNaN(size)) {
     console.log('X'.repeat(size));
   }
 }
-
-node 8-square.js
-# Missing size
-
-node 8-square.js School
-# Missing size
-
-node 8-square.js 2
-# XX
-# XX
-
-node 8-square.js 6
-# XXXXXX
-# XXXXXX
-# XXXXXX
-# XXXXXX
-# XXXXXX
-# XXXXXX
-
-node 8-square.js -3
-# (prints nothing — accepted)
